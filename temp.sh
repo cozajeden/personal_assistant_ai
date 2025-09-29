e34a9cee9b145f58e9d8dc3d4d6af683033c0fcd
Ryszard Napierała <cozajeden@gmail.com>
2025-09-29 19:42:11 +0200

    clearing trash

--- temp.sh
+++
@@ -1,6 +0,0 @@
-docker exec ollama ollama run deepseek-coder:6.7b \
-&& docker exec ollama ollama run deepseek-coder:1.3b \
-&& docker exec ollama ollama run deepseek-coder:1.3b \
-&& docker exec ollama ollama run phi3:3.8b \
-&& docker exec ollama ollama run tinyllama:1.1b \
-&& docker exec ollama ollama run deepseek-llm:7b
